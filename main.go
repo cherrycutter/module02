70cac72213e11de5bc4d78db382ff310703150c6
cherrycutter <werty2.pop@gmail.com>
2024-02-23 20:36:47 +0200

    Completed first task

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Println("Hello2")
+	str := "100"
+	num := 50
+
+	strToNum, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	numToStr := strconv.Itoa(num)
+
+	fmt.Printf("Строка '%s' теперь число %d\n", str, strToNum)
+	fmt.Printf("Число %d теперь строка '%s'\n", num, numToStr)
+	fmt.Printf("%T\n", strToNum)
+	fmt.Printf("%T\n", numToStr)
+
 }
